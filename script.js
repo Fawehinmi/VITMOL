e3cec677c9c1390fc8762a3426c0ecb32427aa48
Fawehinmi Emmanuel <emmanuelfawehinmi12@gmail.com>
2022-05-03 22:16:58 +0100

    update

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 // document.documentElement.scrollHeight - window.innerHeight
 
+const showUpButton = () => {
+  if (
+    document.body.scrollTop > 450 ||
+    document.documentElement.scrollTop > 450
+  ) {
+    document.querySelector(".up").classList.contains("hidden") &&
+      document.querySelector(".up").classList.remove("hidden");
+  } else {
+    document.querySelector(".up").classList.add("hidden");
+  }
+};
 
-window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
-        document.querySelector('.up').classList.contains('hidden') && document.querySelector('.up').classList.remove('hidden')
-    } else {
-        document.querySelector('.up').classList.add('hidden')
-    }
-})
+const closeNav = () => {
+  if (document.querySelector(".mobile-nav").classList.contains("none")) {
+    return;
+  } else {
+    document.querySelector(".mobile-nav").classList.add("none");
+  }
+};
+
+window.addEventListener("scroll", () => {
+  showUpButton();
+  closeNav();
+});
+
+document.querySelector(".nav-toggler").addEventListener("click", () => {
+  if (document.querySelector(".mobile-nav").classList.contains("none")) {
+    document.querySelector(".mobile-nav").classList.remove("none");
+  } else {
+    document.querySelector(".mobile-nav").classList.add("none");
+  }
+});
